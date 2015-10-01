defc0c07cffa7e104f78aa432e753b87aee7f30c
Nathaniel Kitzke <nat.kitzke@gmail.com>
2015-10-01 22:15:29 +0000

    challenge 1

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+alert("Hello World");
+});
+
+$.getJSON("https://loopback-rest-api-workshop-nk1tz.c9.io/api/AddressBooks",
+function(data) {
+    console.log(data);
+    var addressBookNames = data.map( function (ele, ind){
+        return ele.name; 
+    });
+    for(var i in addressBookNames){
+        $("div").append("<p>"+addressBookNames[i]+"</p>");
+    }
+   
+})
